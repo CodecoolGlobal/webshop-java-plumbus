887f40c564ec928318963cc731065e23e2196cc5
Gnorbi951 <gnorbi951@gmail.com>
2020-02-05 11:27:06 +0100

    Add 3 new products

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -30,6 +30,7 @@ public class Initializer implements ServletContextListener {
         Supplier arduino = new Supplier("Arduino", "Micro controllers");
         Supplier asus = new Supplier("ASUS", "Notebooks");
         Supplier apple = new Supplier("Apple", "Consumer electronics");
+        Supplier gigabyte = new Supplier("Gigabyte", "Computer parts");
         supplierDataStore.add(amazon);
         supplierDataStore.add(lenovo);
         supplierDataStore.add(raspberry);
@@ -38,17 +39,14 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(apple);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         ProductCategory sbc = new ProductCategory("Single Board Computer", "Hardware", "Single board computers are used for lightweight tasks, they need very little amount of power.");
         ProductCategory notebook = new ProductCategory("Notebook", "Hardware", "Notebooks are reliable way of having a computer with you on the go.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory gpu = new ProductCategory("Graphics Card", "Hardware", "Graphics card are mainly used for gaming, it boosts the graphical processing power of the computer");
         productCategoryDataStore.add(sbc);
         productCategoryDataStore.add(notebook);
+        productCategoryDataStore.add(gpu);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
         productDataStore.add(new Product("Raspberry Pi 3B", 48.85f, "USD", "The Raspberry Pi 3 Model B is the earliest model of the third-generation Raspberry Pi.", sbc, raspberry));
         productDataStore.add(new Product("Raspberry Pi 3B+", 55.96f, "USD", "The Raspberry Pi 3 Model B+ is the highest performing model in this generation of SBC-s", sbc, raspberry));
         productDataStore.add(new Product("Raspberry Pi 4B [4GB]", 76.99f, "USD", "You'll recognise the price along with the basic shape and size, so you can simply drop your new Raspberry Pi into your old projects for an upgrade", sbc, raspberry));
@@ -57,6 +55,12 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Lenovo Legion Y540", 998.99f, "USD", "This gaming laptop has the top of the line performance for the best price on the market right now, you games definetly won't lag.", notebook, lenovo));
         productDataStore.add(new Product("ASUS ROG Strix G531GT", 999.9f, "USD", "Join the Republic Of Gamers with this laptop, its cooling solution will definetly last longer then your gaming session", notebook, asus));
         productDataStore.add(new Product("Apple MacBook Pro 16", 2999.9f, "USD", "Apple's 16-inch MacBook Pro is basically every creative’s dream machine, with a ton of power and a vastly improved keyboard.", notebook, apple));
+        productDataStore.add(new Product("Raspberry Pi 3A+", 24.9f, "USD", "The Raspberry Pi 3 Model A+ is a smaller version of the B model, with less processing power, but it's nice if you're in need of space", sbc, raspberry));
+        productDataStore.add(new Product("Gigabyte GeForce GTX 1660 Ti OC 6GB", 319.9f, "USD", "This unit has a Windforce cooling system, that keeps the gpu cool for long sessions.", gpu, gigabyte));
+        productDataStore.add(new Product("GIGABYTE GeForce GTX 1050 Ti OC 4GB", 165, "USD", "Most popular choice among low budget gamers", gpu, gigabyte));
+        productDataStore.add(new Product("GIGABYTE GeForce RTX 2060 SUPER AORUS 8GB", 519.9f, "USD", "Nice looking Graphics Card with RGB lighting", gpu, gigabyte));
+
+
 
     }
 }
