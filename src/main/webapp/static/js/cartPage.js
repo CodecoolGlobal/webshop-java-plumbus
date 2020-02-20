7f3beb131c73adeb52168873688aabe4d3df50e8
Gnorbi951 <gnorbi951@gmail.com>
2020-02-20 11:32:28 +0100

    Add style to login and register buttons

--- src/main/webapp/static/js/cartPage.js
+++ src/main/webapp/static/js/cartPage.js
@@ -4,10 +4,32 @@ function collectPrices() {
     for (let value of prices) {
         total+= parseInt(value.innerText);
     }
+
     return total;
 }
+function getCartContent() {
+    fetch('/api/get_cart').then(response => response.json())
+        .then(response => console.log(response));
+}
 
-function buildPage(price) {
+function buildPage(cartContent) {
+    const toAppend = document.querySelector(".cart_header");
+    console.log(cartContent[0].name);
+    let items = "";
+    for (let item of cartContent) {
+        items += `<div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text price">Price: <span class="unit_price" ${item.price}></span></p>
+                <p class="card-text"><small class="text-muted"><span ${item.quantity}></span></small></p>
+            </div>
+        </div>`;
+    }
+    let nodeItems = document.createRange().createContextualFragment(items);
+    toAppend.appendChild(nodeItems);
+}
+
+function showTotal(price) {
     let totalCard = `
             <div class="card js_total">
                 <div class="card-body">
@@ -20,10 +42,10 @@ function buildPage(price) {
     appendTo.appendChild(nodeCard);
 }
 
-
-
 function main() {
+    getCartContent();
     let price = collectPrices();
-    buildPage(price);
+    showTotal(price);
 }
+
 main();
