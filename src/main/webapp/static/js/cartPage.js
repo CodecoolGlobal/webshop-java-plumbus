127f5dee184e4c649df06211b8b3c4ca409be489
Gnorbi951 <gnorbi951@gmail.com>
2020-03-04 12:05:26 +0100

    count items on page

--- src/main/webapp/static/js/cartPage.js
+++ src/main/webapp/static/js/cartPage.js
@@ -7,7 +7,42 @@ function collectPrices() {
     return total;
 }
 
-function buildPage(price) {
+
+function collectQuantities() {
+    let items = {"Raspberry Pi 3B": 0,
+                "Raspberry Pi 3B+": 0,
+                "Raspberry Pi 4B [4GB]": 0,
+                "Arduino Uno R3": 0,
+                "Raspberry Pi 3A+": 0,
+                "Lenovo IdeaPad S145": 0,
+                "Lenovo Legion Y540": 0,
+                "ASUS ROG Strix G531GT": 0,
+                "Apple MacBook Pro 16": 0,
+                "Gigabyte GeForce GTX 1660 Ti OC 6GB": 0,
+                "GIGABYTE GeForce GTX 1050 Ti OC 4GB": 0,
+                "GIGABYTE GeForce RTX 2060 SUPER AORUS 8GB": 0,
+                "AMD Ryzen 5 3600 Hexa-Core 3.6GHz AM4 Processor": 0,
+                "AMD Ryzen 7 3700x Octa-Core 3.6GHz AM4": 0};
+    /*
+    for (let item in items) {
+        console.log(item + items[item]);
+    }
+     */
+
+    let titles = document.querySelectorAll(".card-title");
+    for (let title of titles) {
+        for (let item in items) {
+            if (item === title.innerText) {
+                items[item] += 1;
+            }
+        }
+    }
+    return items;
+}
+
+
+
+function printOutPrices(price) {
     let totalCard = `
             <div class="card js_total">
                 <div class="card-body">
@@ -23,7 +58,8 @@ function buildPage(price) {
 
 
 function main() {
+    let pageContent = collectQuantities();
     let price = collectPrices();
-    buildPage(price);
+    printOutPrices(price);
 }
 main();
