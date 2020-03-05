df165410a173c362775bdadf690709a5acb4f542
David <davmmm@gmail.com>
2020-03-05 19:29:32 +0100

    Add checkout page

--- src/main/java/com/codecool/shop/controller/CheckOut.java
+++ src/main/java/com/codecool/shop/controller/CheckOut.java
@@ -13,15 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(urlPatterns = {"/cart"})
-public class CartPage extends HttpServlet {
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckOut extends HttpServlet {
     private CartDaoMem cartDaoMem = CartDaoMem.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameterNames().hasMoreElements()) {
+            resp.sendRedirect("/");
+        }
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("items", cartDaoMem.getAll());
-        engine.process("product/cart.html", context, resp.getWriter());
+        engine.process("product/checkout.html", context, resp.getWriter());
     }
 }
